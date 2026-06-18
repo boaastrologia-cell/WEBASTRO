fc126d9d22cfd72c13eedfca7c2104a92ac15288
boaastrologia-cell <boaastrologia@gmail.com>
2026-06-18 18:08:04 -0300

    feat: adicionar arquivos __init__.py para os pacotes

---
+++ config/__init__.py
@@ -0,0 +1,21 @@
+"""
+Pacote de configurações do WEBASTRO
+"""
+
+from .settings import (
+    ENVIRONMENT,
+    DEBUG,
+    ODISSEU_CONFIG,
+    FLASK_PORT,
+    FLASK_HOST,
+    CONFIG_SUMMARY,
+)
+
+__all__ = [
+    "ENVIRONMENT",
+    "DEBUG",
+    "ODISSEU_CONFIG",
+    "FLASK_PORT",
+    "FLASK_HOST",
+    "CONFIG_SUMMARY",
+]
---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+Pacote de integração e módulos do WEBASTRO
+"""
+
+from .odisseu_integration import OdisseuAgent, OdisseuError, OdisseuConnectionError
+
+__all__ = [
+    "OdisseuAgent",
+    "OdisseuError",
+    "OdisseuConnectionError",
+]
